f19bd4b8bc960206e075f536927052d1bd6a97f6
ocanades <ocanades@gmail.com>
2020-10-06 13:50:54 +0200

    exercise 9 final commit

--- exer-09/app.js
+++ exer-09/app.js
@@ -1,16 +1,7 @@
-function starRange(star) {
-
-    var stars = []; 
-    stars.push(
-        document.getElementById('star1'),
-        document.getElementById('star2'),
-        document.getElementById('star3'),
-        document.getElementById('star4'),
-        document.getElementById('star5')
-        );
-
-    for(i=4; i>=parseInt(star);i--) {
-        stars[i].style.color = '#f1c40f';
-    } 
-      
-}
+window.addEventListener('click', e => { 
+    if (!document.getElementById('rating-group').contains(e.target)) {
+        for (i=1; i<=5; i++) {
+            $('#star'+[i]).prop('checked', false);
+        }
+    }
+});
